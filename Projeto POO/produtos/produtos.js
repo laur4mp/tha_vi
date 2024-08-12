024ac28bb8332e01b83094f7812f318cbce772be
Laura Maria <162618856+laur4mp@users.noreply.github.com>
2024-08-12 13:05:35 -0300

    Add files via upload

---
+++ Projeto POO/produtos/produtos.js
@@ -0,0 +1,12 @@
+function Layout() {
+    window.location.href = 'Layoutpág.html';
+}
+
+function Contato() {
+    window.location.href = 'contato.html'; 
+}
+
+function Compre() {
+    window.location.href = 'contato.html'; 
+}
+
